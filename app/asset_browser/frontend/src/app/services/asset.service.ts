dac08c08a61945d9677202a07d13437bab064761
eylong <eylong@google.com>
2020-05-10 15:53:33 +0300

    assets for demo remove

--- app/asset_browser/frontend/src/app/services/asset.service.ts
+++ app/asset_browser/frontend/src/app/services/asset.service.ts
@@ -19,60 +19,52 @@ export class AssetService {
 
   getAllAssets() {
     return [
-        {
-          id: 8771117953,
-          name: '',
-          type: 'TEXT',
-          asset_text: 'See who blocked you',
-        },
-        {
-          id: 8771117956,
-          name: '',
-          type: 'TEXT',
-          asset_text: 'Who interact with your profile?',
-        },
-        {
-          id: 8771117962,
-          name: '',
-          type: 'TEXT',
-          asset_text: 'Discover who unfollow you',
-        },
-        {
-          id: 8771117941,
-          name: '',
-          type: 'TEXT',
-          asset_text: 'View stories secretly',
-        },
-        {
-          id: 8788897311,
-          name: 'AG112-EN-L',
-          type: 'YoutubeVideoAsset',
-          video_id: 'cxMnkPYj7T8',
-          link: 'https://www.youtube.com/watch?v=cxMnkPYj7T8',
-          url: 'https://img.youtube.com/vi/cxMnkPYj7T8/1.jpg',
-        },
-        {
-          id: 8788897326,
-          name: 'AG110-EN-K',
-          type: 'YoutubeVideoAsset',
-          video_id: 'VmxNSyUqmm0',
-          link: 'https://www.youtube.com/watch?v=VmxNSyUqmm0',
-          url: 'https://img.youtube.com/vi/VmxNSyUqmm0/1.jpg',
-        },
-        {
-          id: 8771117995,
-          name: 'AG29-EN-K',
-          type: 'YoutubeVideoAsset',
-          video_id: 'Wu2BNazy41Y',
-          link: 'https://www.youtube.com/watch?v=Wu2BNazy41Y',
-          url: 'https://img.youtube.com/vi/Wu2BNazy41Y/1.jpg',
-        },
-        {
-          id: 8807432843,
-          name: 'EN_20200429_AG124_IMG_20.jpg',
-          type: 'ImageAsset',
-          url: 'https://tpc.googlesyndication.com/simgad/16824038254193447182',
-        },
+      {
+        id: 8170555646,
+        name: 'video_file.jpg',
+        type: 'YoutubeVideoAsset',
+        video_id: 'ukjDKYS8gPM',
+        link: 'https://www.youtube.com/watch?v=ukjDKYS8gPM',
+        url: 'https://img.youtube.com/vi/ukjDKYS8gPM/1.jpg',
+        adgroups: [97909190375, 97982663179],
+      },
+      {
+        id: 8037638620,
+        name: '',
+        type: 'TEXT',
+        asset_text: 'Become a virtual Gardner',
+      },
+      {
+        id: 8170555649,
+        name: 'None',
+        type: 'YoutubeVideoAsset',
+        video_id: 'MkOlifBjmBI',
+        link: 'https://www.youtube.com/watch?v=MkOlifBjmBI',
+        url: 'https://img.youtube.com/vi/MkOlifBjmBI/1.jpg',
+        adgroups: [97982663179],
+      },
+      {
+        id: 8048065853,
+        name: 'ph.jpg',
+        type: 'ImageAsset',
+        url: 'https://tpc.googlesyndication.com/simgad/14098414386513970705',
+        adgroups: [95186899405],
+      },
+      {
+        id: 8315727242,
+        name: 'video1',
+        type: 'YoutubeVideoAsset',
+        video_id: 'ZRCdORJiUgU',
+        link: 'https://www.youtube.com/watch?v=ZRCdORJiUgU',
+        url: 'https://img.youtube.com/vi/ZRCdORJiUgU/1.jpg',
+        adgroups: [95186899405],
+      },
+      {
+        id: 8037638617,
+        name: '',
+        type: 'TEXT',
+        asset_text: 'Loads of fun!',
+      },
     ];
   }
 
