9eb1ae6a5f44ae18a550a8a6e09f51bd268b9138
Mariam Youssef <mariamyoussef@google.com>
2020-05-19 17:57:48 +0400

    Fixed the loader issue
    Change-Id: I987a75c503935e202c7ce4bee3b6dd2e2e9fc688

--- app/asset_browser/frontend/src/app/interceptors/loader.interceptor.ts
+++ app/asset_browser/frontend/src/app/interceptors/loader.interceptor.ts
@@ -10,15 +10,22 @@ import { finalize } from 'rxjs/operators';
 import { LoaderService } from '../services/loader.service';
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
+  private totalRequests = 0;
+
   constructor(public loaderService: LoaderService) {}
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    this.totalRequests++;
     this.loaderService.show();
+    
     return next.handle(req).pipe(
       finalize(() => {
-        this.loaderService.hide();
+        this.totalRequests--;
+        if (this.totalRequests === 0) {
+          this.loaderService.hide();
+        }
       })
     );
   }
